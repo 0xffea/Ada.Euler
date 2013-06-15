5b4a31040e0293bae25ffc60abbe187124aedd67
David Höppner <0xffea@gmail.com>
2013-06-15 18:59:13 +0200

    Correct #2

--- source/euler-002.adb
+++ source/euler-002.adb
@@ -1,10 +1,16 @@
+--
+-- http://oeis.org/A000045
+--
+
 with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Assertions; use Ada.Assertions;
+
 
 procedure Euler_002 is
    
    function E002_01 (N : Natural) return Natural is
       Fib_Prev : Natural := 1;
-      Fib_Next : Natural := 2;
+      Fib_Next : Natural := 1;
    begin
       return Sum : Natural := 0 do
 	loop
@@ -24,6 +30,29 @@ procedure Euler_002 is
       end return;
    end E002_01;
    
+   --
+   -- 2|f_n <=> 3|n
+   --
+   function E002_02 (N : Natural) return Natural is
+      Fib_A : Natural := 1;
+      Fib_B : Natural := 1;
+      Fib_C : Natural := 2;
+   begin
+      return Sum : Natural := 2 do
+	loop
+	   Fib_A := Fib_B + Fib_C;
+	   Fib_B := Fib_A + Fib_C;
+	   Fib_C := Fib_A + Fib_B;
+	   
+	   exit when Fib_C > N;
+	   
+	   Sum := Sum + Fib_C;
+	end loop;
+      end return;
+   end E002_02;
+   
 begin
-   Put_Line(Natural'Image (E002_01(4_000_000)));
+   Assert (E002_01(4_000_000) = E002_02(4_000_000));
+   
+   Put_Line (Natural'Image (E002_01(4_000_000)));
 end Euler_002;
