619413e6f0c4635ba7e9bac90792883dcfc5a214
David Höppner <0xffea@gmail.com>
2013-06-15 09:42:15 +0200

    Add literature #1

--- source/euler-001.adb
+++ source/euler-001.adb
@@ -1,10 +1,27 @@
+--
+-- Multiples of 3 and 5
+--
+-- If we list all the natural numbers below 10 that are multiples
+-- of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+--
+-- Find the sum of all the multiples of 3 or 5 below 1000.
+--
+--
+-- RAYMOND T. BOUTE: The Euclidean Definition of the Functions div
+-- and mod. ACM Transactions on Programming Languages and Systems
+-- 14.2 (1992):127-144
+-- DAAN LEIJEN: Division and Modulus for Computer Scientists.
+-- BOYKO B. BANTCHEV: On the Definition of Integer Division and Modulus
+-- in Programming Languages. Mathematics and Education (2006):223-229
+--
+
 with Ada.Text_IO;
 with Ada.Assertions;
 
 procedure Euler_001 is
    use Ada.Text_IO;
    use Ada.Assertions;
-
+   
    function E001_01 (N : Natural) return Natural is
    begin
       return Sum : Natural := 0 do
@@ -30,7 +47,7 @@ procedure Euler_001 is
 	    Sum := Sum + I;
 	 end loop;
       end;
-
+	
       declare
 	 I : Natural := 0;
       begin
@@ -42,10 +59,10 @@ procedure Euler_001 is
       end;
       return Sum;
    end E001_02;
-
+   
 begin
    Assert(23 = E001_01(10));
    Assert(23 = E001_02(10));
-
+   
    Put_Line(Natural'Image (E001_01(1_000)));
 end Euler_001;
